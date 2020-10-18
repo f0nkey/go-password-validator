d497649cb2aa6ba1b811ad91534e919f41e47ef1
f0nkey <mystery@gmail.com>
2020-10-18 18:37:36 -0500

    Add preliminary code for removing common substrings; remove old code

--- length.go
+++ length.go
@@ -1,14 +1,19 @@
 package passwordvalidator
 
-import "github.com/lane-c-wagner/go-password-validator/lengthpattern"
-
-// UsePatternPenalty when true, counts substrings that can be connected in a continuous (non-diagonal) line count as zero.
-var UsePatternPenalty = false
+import (
+	"io/ioutil"
+	"log"
+	"strings"
+	"unicode/utf8"
+)
 
 func getLength(password string) int {
-	if UsePatternPenalty {
-		return lengthpattern.GetLength(password)
-	}
+	//cpc := getCountPerChar()
+
+	return 0
+}
+
+func getCountPerChar(password string) map[rune]int {
 	const maxNumSameChar = 2
 	chars := map[rune]int{}
 	for _, c := range password {
@@ -20,9 +25,81 @@ func getLength(password string) int {
 		}
 		chars[c]++
 	}
-	length := 0
-	for _, count := range chars {
-		length += count
-	}
-	return length
+	return chars
 }
+
+func deleteCommonPassSubstrings(passMap passwordsMap, password string) string {
+	// Scan across the string, using the largest scanWidths first
+	scanWidth := len(password)
+	start := 0
+	for scanWidth != 0 && len(password) != 0{
+		for end := start+scanWidth; end <= len(password); {
+			password = deleteSubStringIfBad(passMap, password, start, end)
+			start++
+			end++
+		}
+		scanWidth--
+		start = 0
+	}
+	return password
+}
+
+func deleteSubStringIfBad(passMap passwordsMap, password string, start int, end int) string {
+	subStr := password[start:end]
+	if passMap.exists(subStr) {
+		password = strings.Replace(password,subStr,"",1)
+	}
+	return password
+}
+
+// is Replace from standard lib, but with start argument
+func replaceStart(s, old, new string, n int, start int) string {
+	if old == new || n == 0 {
+		return s // avoid allocation
+	}
+
+	// Compute number of replacements.
+	if m := strings.Count(s, old); m == 0 {
+		return s // avoid allocation
+	} else if n < 0 || m < n {
+		n = m
+	}
+
+	// Apply replacements to buffer.
+	t := make([]byte, len(s)+n*(len(new)-len(old)))
+	w := 0
+	//start := 0
+	for i := 0; i < n; i++ {
+		j := start
+		if len(old) == 0 {
+			if i > 0 {
+				_, wid := utf8.DecodeRuneInString(s[start:])
+				j += wid
+			}
+		} else {
+			j += strings.Index(s[start:], old)
+		}
+		w += copy(t[w:], s[start:j])
+		w += copy(t[w:], new)
+		start = j + len(old)
+	}
+	w += copy(t[w:], s[start:])
+	return string(t[0:w])
+}
+
+type passwordsMap map[string]struct{}
+
+func (pwm passwordsMap) exists(password string) bool {
+	_, exists := pwm[password]
+	return exists
+}
+
+var mostCommonPasswords = func() []string{
+	b, err := ioutil.ReadFile("most-common-passwords.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return strings.Split(string(b),"\n") // consider manually splitting and doing make([]string,0,10000)
+}()
+
--- length_test.go
+++ length_test.go
@@ -1,6 +1,7 @@
 package passwordvalidator
 
 import (
+	"sort"
 	"testing"
 )
 
@@ -16,4 +17,109 @@ func TestGetLength(t *testing.T) {
 	if actual != expected {
 		t.Errorf("Wanted %v, got %v", expected, actual)
 	}
+}
+
+func TestDeleteCommonPassSubstrings(t *testing.T) {
+	actual := deleteCommonPassSubstrings(pm, "password")
+	expected := ""
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+
+	actual = deleteCommonPassSubstrings(pm, "oglrpassword")
+	expected = "oglr"
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+
+	actual = deleteCommonPassSubstrings(pm, "gtmsupermangtmo#78")
+	expected = "gtmgtmo#78"
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+
+	// should actually be a high entropy password, maybe should not punish for it
+	actual = deleteCommonPassSubstrings(pm, "qwertyuiopfootballsuperman")
+	expected = ""
+	if actual != expected {
+		t.Errorf("Wanted %v, got %v", expected, actual)
+	}
+}
+
+var pbl = genPasswordsByLength(mostCommonPasswords)
+// Does splitting the passwords into maps ordered by password length help with lookup performance?
+func BenchmarkLookupPasswordByLength(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		pblExists(pbl,"football")
+		pblExists(pbl,"password")
+		pblExists(pbl,"letmein")
+		pblExists(pbl,"love")
+		pblExists(pbl,"qwertyuiop")
+	}
+}
+
+func pblExists(pbl map[int]passwordsMap, password string) bool {
+	if pbl[len(password)] == nil {
+		return false
+	}
+	return pbl[len(password)].exists(password)
+}
+
+var pm = genPasswordsOnly(mostCommonPasswords)
+// Does using a normal map to see existence work well enough?
+func BenchmarkLookupPasswordMap(b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		pm.exists("football")
+		pm.exists("password")
+		pm.exists("letmein")
+		pm.exists("love")
+		pm.exists("qwertyuiop")
+	}
+}
+
+// Does using a bloom filter do better?
+//func BenchmarkLookupBloom(b *testing.B) {
+//	for n := 0; n < b.N; n++ {
+//		// bloom code
+//	}
+//}
+
+// https://gobyexample.com/sorting-by-functions
+type byLength []string
+
+func (s byLength) Len() int {
+	return len(s)
+}
+func (s byLength) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+func (s byLength) Less(i, j int) bool {
+	return len(s[i]) < len(s[j])
+}
+
+// Used in benchmarking
+func genPasswordsByLength(passwords []string) map[int]passwordsMap {
+	passwordsByLength := make(map[int]passwordsMap)
+
+	sort.Sort(byLength(passwords))
+	for _, password := range passwords {
+		if passwordsByLength[len(password)] == nil {
+			passwordsByLength[len(password)] = make(passwordsMap)
+		}
+		passMap := passwordsByLength[len(password)]
+		passMap[password] = struct{}{}
+	}
+
+	return passwordsByLength
+}
+
+// Used in benchmarking
+func genPasswordsOnly(passwords []string) passwordsMap {
+	passwordsMap := make(passwordsMap, 10000)
+	sort.Sort(byLength(passwords))
+	for _, password := range passwords {
+		passwordsMap[password] = struct{}{}
+	}
+
+	return passwordsMap
 }
